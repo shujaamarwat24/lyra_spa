8213cc4aa236013d47e019071554770fabc14d09
Shujaa Marwat <shujaa.khan@devsinc.com>
2023-03-14 20:50:07 +0500

    Added functionality for drag/select  shapes: * index.jsx (Editor): Refactorred code to make it less boilerplate. As well as add event handling for moving shapes * index.jsx(Editor): Added functionality to handle movement

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -4,35 +4,68 @@ import './style.scss';
 
 const Editor = ({ activeTool }) => {
   const [shapes, setShapes] = useState([]);
+  const [selectedShapeIndex, setSelectedShapeIndex] = useState(null);
 
   const handleCanvasClick = (event) => {
     if (activeTool === "add-square") {
-      const newShape = { type: "square", x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY };
+      const newShape = {
+        type: "square",
+        x: event.nativeEvent.offsetX,
+        y: event.nativeEvent.offsetY,
+      };
       setShapes([...shapes, newShape]);
     } else if (activeTool === "add-triangle") {
-      const newShape = { type: "triangle", x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY };
+      const newShape = {
+        type: "triangle",
+        x: event.nativeEvent.offsetX,
+        y: event.nativeEvent.offsetY,
+      };
       setShapes([...shapes, newShape]);
     } else if (activeTool === "add-hexagon") {
-      const newShape = { type: "hexagon", x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY };
+      const newShape = {
+        type: "hexagon",
+        x: event.nativeEvent.offsetX,
+        y: event.nativeEvent.offsetY,
+      };
       setShapes([...shapes, newShape]);
     }
   };
 
   const handleShapeClick = (clickedShapeIndex) => {
-    const clickedShape = shapes.splice(clickedShapeIndex, 1)[0];
-    setShapes([...shapes, clickedShape]);
+    if (activeTool === "move") {
+      setSelectedShapeIndex(clickedShapeIndex);
+    }
+  };
+
+  const handleShapeMove = (event) => {
+    if (selectedShapeIndex !== null) {
+      const updatedShapes = [...shapes];
+      updatedShapes[selectedShapeIndex].x = event.nativeEvent.offsetX;
+      updatedShapes[selectedShapeIndex].y = event.nativeEvent.offsetY;
+      setShapes(updatedShapes);
+    }
   };
 
   const renderedShapes = shapes.map((shape, index) => (
-
-    <Shape key={index} index={index} shape={shape} onClick={() => handleShapeClick(index)} />
+    <Shape
+      key={index}
+      index={index}
+      shape={shape}
+      onClick={() => handleShapeClick(index)}
+      selected={index === selectedShapeIndex}
+    />
   ));
 
   return (
-    <div className="editor" onClick={handleCanvasClick}>
+    <div
+      className="editor"
+      onClick={handleCanvasClick}
+      onMouseMove={handleShapeMove}
+    >
       {renderedShapes}
     </div>
   );
 };
+
 
 export default Editor;
--- src/components/Shape/index.jsx
+++ src/components/Shape/index.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import { useState, useRef } from "react";
 
-const Shape = ({ shape, onClick,index }) => {
-  const renderShape = () => {
-    switch (shape.type) {
-      case "square":
-        return <div className="square shape" id={index} style={{ left: shape.x, top: shape.y }} onClick={onClick}></div>;
-      case "triangle":
-        return <div className="triangle shape" id={index} style={{ left: shape.x, top: shape.y }} onClick={onClick}></div>;
-      case "hexagon":
-        return <div className="hexagon shape" id={index} style={{ left: shape.x, top: shape.y }} onClick={onClick}></div>;
-      default:
-        return null;
+const Shape = ({ shape, onClick, index, selected }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: shape.x, y: shape.y });
+
+  const shapeRef = useRef(null);
+
+  const handleMouseDown = (event) => {
+    event.stopPropagation();
+    setIsDragging(true);
+  };
+
+  const handleMouseMove = (event) => {
+    if (isDragging) {
+      const offsetX = event.clientX - shapeRef.current.offsetWidth / 2;
+      const offsetY = event.clientY - shapeRef.current.offsetHeight / 2;
+      setPosition({ x: offsetX, y: offsetY });
     }
   };
 
-  return renderShape();
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    shape.x = position.x;
+    shape.y = position.y;
+  };
+
+  return (
+    <div
+      ref={shapeRef}
+      className={`${shape.type} shape ${selected ? "selected" : ""}`}
+      id={index}
+      style={{ left: position.x, top: position.y }}
+      onClick={onClick}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    ></div>
+  );
 };
 
 export default Shape;
